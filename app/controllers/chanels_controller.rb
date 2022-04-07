68c8022e89acd329c737d58d49346ab714e95266
Chestnut-TH <t.hirai.1997.0328.chestnut@gmail.com>
2022-04-08 04:12:14 +0900

    チャンネル登録機能

--- app/controllers/chanels_controller.rb
+++ app/controllers/chanels_controller.rb
@@ -11,4 +11,53 @@ class ChanelsController < ApplicationController
     .or(Chanel.where(tag4: @tag))
     .or(Chanel.where(tag5: @tag))    
   end
+
+   
+  def new
+    @chanel = Chanel.new  
+  end
+
+  def create
+    if current_user
+      chanel_regist
+    else
+      flash.now[:danger] = 'ログインしてません'
+      redirect_to render new_user_session_path
+    end
+  end
+
+  def show
+    @chanel = Chanel.find(params[:id]);
+  end
+
+  private
+    def chanel_regist
+      @chanel = Chanel.new (chanel_params)
+      if @chanel.save
+        tag_regist
+        redirect_to @chanel
+      else
+        render new_chanel
+      end
+    end
+
+    def chanel_params
+      params.require(:chanel).permit(:name, :url, :tag1, :tag2, :tag3, :tag4, :tag5)
+    end
+
+    def tag_regist
+      tags = [tag_params[:tag1],tag_params[:tag2],tag_params[:tag3],tag_params[:tag4],tag_params[:tag5]]
+      tags.each{|tag|
+        if tag != "" && !tag.nil? then
+          count = Tag.where(name: tag).count
+          if count<= 0 then
+            Tag.create(name:tag);
+          end
+        end        
+      }
+    end
+
+    def tag_params
+      params.require(:chanel).permit(:name, :url, :tag1, :tag2, :tag3, :tag4, :tag5)
+    end
 end
--- app/models/chanel.rb
+++ app/models/chanel.rb
@@ -1,2 +1,5 @@
 class Chanel < ApplicationRecord
+    validates :name, presence: true
+    validates :url, presence: true
+    validates :tag1, presence: true
 end
